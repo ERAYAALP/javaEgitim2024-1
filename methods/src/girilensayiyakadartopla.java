730af3a6710a40926cfc8fc50847fa871edbaabf
ERAYAALP <erayaalp@gmail.com>
2024-01-14 14:11:16 +0300

    method_examples

---
+++ methods/src/girilensayiyakadartopla.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class girilensayiyakadartopla {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int sayi1,sonuc;
+        System.out.println("Bir sayı giriniz: ");
+        sayi1=scanner.nextInt();
+        sonuc = hesapla(sayi1);
+        System.out.println(sayi1+ " girilen sayıya kadar olan sayıların toplamı: " +sonuc);
+
+    }
+    public static int hesapla(int sayi1){
+        int toplam=0;
+        for (int i=1;i<=sayi1;i++)
+        {
+            toplam=toplam+i;
+        }
+        return toplam;
+    }
+}
+
+
---
+++ methods/src/methodlarla4islem.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class methodlarla4islem {
+    public static void main(String[] args) {
+        int number1,number2;
+        Scanner scanner  =new Scanner(System.in);
+        System.out.println("Birinci sayıyı giriniz: ");
+        number1=scanner.nextInt();
+        System.out.println("İkinci sayıyı giriniz: ");
+        number2=scanner.nextInt();
+        toplama(number1,number2);
+        cikarma(number1,number2);
+        carpma(number1,number2);
+        bolme(number1,number2);
+
+
+        }
+        public static void toplama(int s1,int s2)
+        {
+           int  toplam=s1+s2;
+            System.out.println("İki sayının toplamı: " +toplam);
+
+        }
+        public static void cikarma(int s1,int s2){
+        int cikan=s1-s2;
+            System.out.println("İki sayının farkı: " +cikan);
+        }
+        public static void carpma(int s1,int s2)
+        {
+           int carpma=s1*s2;
+            System.out.println("İki sayının çarpımı: "+ carpma);
+
+        }
+        public static void bolme(int s1,int s2){
+        int bolme=s1/s2;
+            System.out.println("İki sayının bölümü: " +bolme);
+        }
+    }
